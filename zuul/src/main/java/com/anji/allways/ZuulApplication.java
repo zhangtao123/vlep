db7fa49e146a12fa54ac038958ed9f7eee526d95
zhangtao <zhangtao123>
2018-05-30 18:18:12 +0800

    新增获取ip地址服务

--- zuul/src/main/java/com/anji/allways/ZuulApplication.java
+++ zuul/src/main/java/com/anji/allways/ZuulApplication.java
@@ -1,12 +1,22 @@
 package com.anji.allways;
 
-import org.springframework.boot.SpringApplication;
+import com.anji.allways.filter.AccessFilter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class, args);
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
---
+++ zuul/src/main/java/com/anji/allways/filter/AccessFilter.java
@@ -0,0 +1,33 @@
+package com.anji.allways.filter;
+
+import com.netflix.zuul.ZuulFilter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 2018/5/30
+ * Time: 16:41
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        return null;
+    }
+}
