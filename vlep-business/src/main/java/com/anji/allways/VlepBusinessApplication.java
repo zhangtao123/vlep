65b159b9b8697400c569c20829f95fc18fcd6065
XuYibo <xuyibo@outlook.com>
2018-05-30 16:02:29 +0800

    配置数据源

--- vlep-business/src/main/java/com/anji/allways/VlepBusinessApplication.java
+++ vlep-business/src/main/java/com/anji/allways/VlepBusinessApplication.java
@@ -1,11 +1,14 @@
 package com.anji.allways;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.anji.allways.dao")
+@EnableTransactionManagement
 public class VlepBusinessApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VlepBusinessApplication.class, args);
     }
